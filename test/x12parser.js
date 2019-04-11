97f783b3430b009bced53c5cb647e3bf93f2c7c4
Ezekiel Keator <20361796+tastypackets@users.noreply.github.com>
2019-04-11 16:53:14 -0500

    tests

--- test/x12parser.js
+++ test/x12parser.js
@@ -3,17 +3,28 @@ const assert = require('assert');
 const { createReadStream } = require('fs');
 
 describe('X12parser', function () {
-  describe('#constructor()', function () {
-    const myParser = new X12parser();
-    it('Shuld return a X12parser', function () {
-      assert(myParser instanceof X12parser);
+    describe('#constructor()', function () {
+        const myParser = new X12parser();
+        it('Shuld return a X12parser', function () {
+            assert(myParser instanceof X12parser);
+        });
+        it('Shuld have a pipe function', function () {
+            assert.strictEqual(typeof myParser.pipe, 'function');
+        });
+        it('Shuld return an event emitter', function () {
+            assert(myParser instanceof require("events").EventEmitter);
+        });
     });
-  });
-  describe('#pipe', function () {
-    const myParser = new X12parser();
-    it('Shuld be able to pipe in data', function () {
-      const testFile = createReadStream('./test/testFiles/profee.edi')
-      testFile.pipe(myParser).on('data', data => console.log(data))
+    describe('835 File w/ CRLF', function () {
+        const myParser = new X12parser();
+        it('Should parse files with CRLF', function () {
+            const testFile = createReadStream('./test/testFiles/835/profee.edi')
+            let counter = 0; // So ugly... This should be done nicer
+            const { finished } = require('./testFiles/835/profee-done');
+            testFile.pipe(myParser).on('data', data => {
+                assert.deepStrictEqual(data, finished[counter])
+                counter++
+            });
+        });
     });
-  });
 });
